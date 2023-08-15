2860fbddb1016743d1add9bc4e438d273287fc40
Imran <imranshkh283@gmail.com>
2023-08-15 14:33:14 +0530

    Implement Interface in Account Class

--- classes/account.ts
+++ classes/account.ts
@@ -1,8 +1,42 @@
+interface Transaction {
+  Amount: number;
+  Date: any;
+  Total: number;
+  Type: string;
+}
+
 export class Account {
   protected balanced: number = 0;
   public withdrawDate: number = 0;
   public depositDate: number = 0;
   protected log: any[] = [];
+
+  transaction(trans: Transaction) {
+    this.log.push({
+      Amount: trans.Amount,
+      Date: this.timelog(),
+      Total: this.balanced,
+      Type: trans.Type,
+    });
+  }
+
+  deposit(amount: number) {
+    this.balanced += amount;
+    const data: Transaction = {
+      Amount: amount,
+      Date: this.timelog(),
+      Total: this.balanced,
+      Type: "deposit",
+    };
+    this.transaction(data);
+    /* this.log.push({
+      Amount: amount,
+      Date: this.timelog(),
+      Total: this.balanced,
+      Type: "deposit",
+    }); */
+    return true;
+  }
 
   withdraw(amount: number) {
     this.balanced -= amount;
@@ -11,17 +45,6 @@ export class Account {
       Date: this.timelog(),
       Total: this.balanced,
       Type: "withdraw",
-    });
-    return true;
-  }
-
-  deposit(amount: number) {
-    this.balanced += amount;
-    this.log.push({
-      Amount: amount,
-      Date: this.timelog(),
-      Total: this.balanced,
-      Type: "deposit",
     });
     return true;
   }
